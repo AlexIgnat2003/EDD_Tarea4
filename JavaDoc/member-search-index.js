2a44b08681e857040604261728816ec16355d42b
AlexIgnat2003 <alejandroignat@gmail.com>
2022-04-26 22:42:44 +0200

    Tercera rama: JavaDoc, completa

---
+++ JavaDoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Primos","l":"generarPrimos(int)"},{"p":"<Unnamed>","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Primos","l":"Primos()","url":"%3Cinit%3E()"}]
